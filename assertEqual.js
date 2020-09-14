78084280c44bcef527e05075f780ad8e0c8cab53
danschae <danschae@gmail.com>
2020-09-14 19:45:56 +0000

    assert exercise

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`"👍" Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`"👎" Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
+assertEqual("lighthouse", "lighthouse");
