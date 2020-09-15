02fcb7966b01c8730266dddc3fd9e0af26f03837
danschae <danschae@gmail.com>
2020-09-15 20:38:16 +0000

    eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`"👍" Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`"👎" Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arrayOne, arrayTwo){
+  if (arrayOne.length !== arrayTwo.length){
+    return false;
+  } else{
+    for (let i = 0; i < arrayOne.length; i++){
+      if(arrayOne[i] !== arrayTwo[i]){
+        return false;
+      }
+    }
+    return true;
+  } 
+}
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+ assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should PASS
