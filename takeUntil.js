1d990429c97f68cf1d18cde9817a0033789341a7
danschae <danschae@gmail.com>
2020-09-17 18:16:54 +0000

    takeuntil method

---
+++ takeUntil.js
@@ -0,0 +1,12 @@
+const takeUntil = function(array, callback) {
+  let slicedArray = [];
+  let index = 0
+  for (const char of array) {
+    if (!callback(char)) {
+      slicedArray = array.slice(0, index += 1)
+    } else{
+      return slicedArray
+    }
+  }
+}
+
